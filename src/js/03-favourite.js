23c62d1c32989d69b2470f5001a69d41be6d3656
Victoria Kovaleva <victoriak.dev@gmail.com>
2021-02-09 23:37:34 +0100

    Add localStorage for favourite movies in JavaScript

--- src/js/03-favourite.js
+++ src/js/03-favourite.js
@@ -5,11 +5,27 @@ const newUl = document.createElement('ul');
 const newUlTitle = document.createTextNode('Mis Series Favoritas:');
 newUl.appendChild(newUlTitle);
 
-function paintFavourites(movieClicked) {
+let favs = []
+const favsData = localStorage.getItem('favs')
+if (favsData !== null) {
+    favs = JSON.parse(favsData)
+}
+
+function addFavourite(myMovie) {
     const newList = document.createElement('li');
-    newList.innerHTML = `${movieClicked.name}<img src="${movieClicked.image}"/>`
+    newList.innerHTML = `${myMovie.name}<img src="${myMovie.image}"/>`
     newUl.appendChild(newList);
     newList.classList.add('js-favourite');
 
     favouriteList.appendChild(newUl);
+}
+
+function paintFavourites(movieClicked) {
+    addFavourite(movieClicked)
+    favs.push(movieClicked);
+    localStorage.setItem('favs', JSON.stringify(favs));
+}
+
+for (let i = 0; i < favs.length; i++) {
+    addFavourite(favs[i]);
 }
